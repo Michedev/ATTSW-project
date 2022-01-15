15b41a6bf97fa8c8e34a117cc464671f7fa54730
mikedev <mik3dev@gmail.com>
2022-01-15 14:59:06 +0100

    add dockerfile for postgres image and add test db including few samples

---
+++ src/main/resources/build_tables.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS TestDB;
+
+CREATE DATABASE TestDB;
+
+DROP TABLE IF EXISTS Users CASCADE;
+DROP TABLE IF EXISTS Tasks;
+
+CREATE TABLE Users (
+        id serial PRIMARY KEY, 
+	username varchar(255),
+	password varchar(255),
+	email varchar(255)
+);
+
+
+
+
+CREATE TABLE Tasks ( 
+        id serial PRIMARY KEY,
+	title varchar(255),
+	description varchar(1000),
+	id_user int,
+	deadline date,
+	done bool,
+	FOREIGN KEY (id_user) REFERENCES Users (id)
+);
