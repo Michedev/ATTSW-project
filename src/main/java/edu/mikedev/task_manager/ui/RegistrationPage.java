093a4c8ff315c8498a54724154cc96f6296d5404
Mikedev <mdv1994@gmail.com>
2022-01-30 12:21:44 +0100

    add specific error label per field

--- src/main/java/edu/mikedev/task_manager/ui/RegistrationPage.java
+++ src/main/java/edu/mikedev/task_manager/ui/RegistrationPage.java
@@ -14,7 +14,7 @@ public class RegistrationPage extends JPanel {
 
     public RegistrationPage(Model model) {
         this.model = model;
-        setLayout(new MigLayout("", "[][][][][]", "[][][][][][][][][][][][][]"));
+        setLayout(new MigLayout("", "[][][][][]", "[][][][][][][][][][][][][][][]"));
 
         JLabel lblUsername = new JLabel("Username");
         lblUsername.setName("lblUsername");
@@ -25,39 +25,51 @@ public class RegistrationPage extends JPanel {
         add(tfUsername, "cell 1 3 2 1,growx");
         tfUsername.setColumns(10);
         
+        JLabel lblErrorMessageUsername = new JLabel("Error message");
+        lblErrorMessageUsername.setName("lblErrorMessageUsername");
+        lblErrorMessageUsername.setVisible(false);
+        lblErrorMessageUsername.setForeground(Color.RED);
+        add(lblErrorMessageUsername, "cell 1 4 2 1");
+        
         JLabel lblPassword = new JLabel("Password");
         lblPassword.setName("lblPassword");
-        add(lblPassword, "cell 1 5");
+        add(lblPassword, "cell 1 6");
         
         tfPassword = new JPasswordField();
         tfPassword.setName("tfPassword");
-        add(tfPassword, "cell 1 6 2 1,growx");
+        add(tfPassword, "cell 1 7 2 1,growx");
+        
+        JLabel lblErrorMessagePassword = new JLabel("Error Message");
+        lblErrorMessagePassword.setVisible(false);
+        lblErrorMessagePassword.setForeground(Color.RED);
+        lblErrorMessagePassword.setName("lblErrorMessagePassword");
+        add(lblErrorMessagePassword, "cell 1 8 2 1");
         
         JLabel lblEmail = new JLabel("E-mail");
         lblEmail.setName("lblEmail");
-        add(lblEmail, "cell 1 8");
+        add(lblEmail, "cell 1 10");
         
         tfEmail = new JTextField();
         tfEmail.setName("tfEmail");
-        add(tfEmail, "cell 1 9 2 1,growx");
+        add(tfEmail, "cell 1 11 2 1,growx");
         tfEmail.setColumns(10);
         
         JLabel lblErrorMessage = new JLabel("Error message");
-        lblErrorMessage.setName("lblErrorMessage");
         lblErrorMessage.setVisible(false);
+        lblErrorMessage.setName("lblErrorMessageEmail");
         lblErrorMessage.setForeground(Color.RED);
-        add(lblErrorMessage, "cell 1 10 2 1");
+        add(lblErrorMessage, "cell 1 12 2 1");
         
         JButton btnRegister = new JButton("Register");
         btnRegister.setName("btnConfirmRegister");
         btnRegister.addActionListener(e ->{
             lblErrorMessage.setVisible(true);
         });
-        add(btnRegister, "cell 1 12");
+        add(btnRegister, "cell 1 14");
         
         JButton btnCancel = new JButton("Cancel");
         btnCancel.setName("btnCancel");
-        add(btnCancel, "cell 2 12");
+        add(btnCancel, "cell 2 14");
 
     }
 }
