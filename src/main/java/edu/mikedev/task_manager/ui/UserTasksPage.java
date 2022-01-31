b93a285d38911d1651e827bf327ea945047a126c
mikedev <mik3dev@gmail.com>
2022-01-31 16:40:37 +0100

    draw task cards based on user tasks

--- src/main/java/edu/mikedev/task_manager/ui/UserTasksPage.java
+++ src/main/java/edu/mikedev/task_manager/ui/UserTasksPage.java
@@ -1,5 +1,6 @@
 package edu.mikedev.task_manager.ui;
 
+import edu.mikedev.task_manager.Task;
 import edu.mikedev.task_manager.User;
 
 import javax.swing.*;
@@ -18,36 +19,43 @@ public class UserTasksPage extends JPanel {
 	public UserTasksPage(User user) {
 		this.user = user;
 		setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
-		
-		JPanel task0 = new JPanel();
-		task0.setMaximumSize(new Dimension(300, 1500000));
-		add(task0);
-		task0.setLayout(new GridLayout(2, 1, 5, 5));
-		
-		JPanel headerPanelTask0 = new JPanel();
-		task0.add(headerPanelTask0);
-		headerPanelTask0.setLayout(new GridLayout(1, 2, 5, 0));
-		
-		JLabel lblTitleTask0 = new JLabel("Task title 1");
+		for(Task t: user.getTasks()){
+			makeTaskCard(t);
+		}
+	}
+
+	private JPanel makeTaskCard(Task t) {
+		JPanel taskPanel = new JPanel();
+		taskPanel.setMaximumSize(new Dimension(300, 1500000));
+		add(taskPanel);
+		taskPanel.setLayout(new GridLayout(2, 1, 5, 5));
+
+		JPanel headerPanelTask = new JPanel();
+		taskPanel.add(headerPanelTask);
+		headerPanelTask.setLayout(new GridLayout(1, 2, 5, 0));
+
+		JLabel lblTitleTask0 = new JLabel(t.getTitle());
 		lblTitleTask0.setFont(new Font("Cantarell", Font.BOLD, 18));
-		lblTitleTask0.setName("lblTitleTask0");
-		headerPanelTask0.add(lblTitleTask0);
-		
-		JLabel lblDateTask0 = new JLabel("05/11/2022");
-		lblDateTask0.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblDateTask0.setFont(new Font("Cantarell", Font.BOLD, 14));
-		lblDateTask0.setName("lblDateTask0");
-		headerPanelTask0.add(lblDateTask0);
-		
-		JPanel contentPanelTask0 = new JPanel();
-		task0.add(contentPanelTask0);
-		contentPanelTask0.setLayout(new GridLayout(1, 0, 0, 0));
-		
-		JLabel lblDescrTask0 = new JLabel("A small task description Lorem Inpsum etc etc...");
-		lblDescrTask0.setMaximumSize(new Dimension(150, 1500));
-		lblDescrTask0.setFont(new Font("Cantarell", Font.BOLD, 12));
-		lblDescrTask0.setName("lblDescrTask0");
-		contentPanelTask0.add(lblDescrTask0);
+		lblTitleTask0.setName("lblTitleTask" + t.getId());
+		headerPanelTask.add(lblTitleTask0);
+
+		JLabel lblDateTask = new JLabel(t.getDeadline().toString());
+		lblDateTask.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblDateTask.setFont(new Font("Cantarell", Font.BOLD, 14));
+		lblDateTask.setName("lblDateTask" + t.getId());
+		headerPanelTask.add(lblDateTask);
+
+		JPanel contentPanelTask = new JPanel();
+		taskPanel.add(contentPanelTask);
+		contentPanelTask.setLayout(new GridLayout(1, 0, 0, 0));
+
+		JLabel lblDescrTask = new JLabel(t.getDescription());
+		lblDescrTask.setMaximumSize(new Dimension(150, 1500));
+		lblDescrTask.setFont(new Font("Cantarell", Font.BOLD, 12));
+		lblDescrTask.setName("lblDescrTask" + t.getId());
+		contentPanelTask.add(lblDescrTask);
+
+		return taskPanel;
 	}
 
 }
