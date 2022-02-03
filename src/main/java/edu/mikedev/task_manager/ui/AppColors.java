e0f2ea137e28599ea9cde355db7a4de32df1e94f
mikedev <mik3dev@gmail.com>
2022-02-03 16:58:12 +0100

    add private default constructor as suggested by sonarqube, add today() method for testing purpuoses

--- src/main/java/edu/mikedev/task_manager/ui/AppColors.java
+++ src/main/java/edu/mikedev/task_manager/ui/AppColors.java
@@ -11,16 +11,22 @@ public class AppColors {
     public static final Color ORANGE = new Color(249,212,35);
     public static final Color GREEN = new Color(225,245,196);
 
+    private AppColors(){}
+
     public static  Color getColorBackground(Task task) {
         if(task.isDone()){
             return AppColors.GREEN;
         } else {
-            if(task.getDeadline().after(Date.from(Instant.now()))){
+            if(task.getDeadline().after(today())){
                 return AppColors.ORANGE;
             } else {
                 return AppColors.RED;
             }
         }
+    }
+
+    public static java.util.Date today() {
+        return Date.from(Instant.now());
     }
 
 }
