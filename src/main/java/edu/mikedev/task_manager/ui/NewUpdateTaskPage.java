3abcad400179209b721e65b440396c902c705702
mikedev <mik3dev@gmail.com>
2022-02-03 17:18:49 +0100

    rename autogenerated windowbuilder variables to follow java variable name conventions, add transient in front of user

--- src/main/java/edu/mikedev/task_manager/ui/NewUpdateTaskPage.java
+++ src/main/java/edu/mikedev/task_manager/ui/NewUpdateTaskPage.java
@@ -22,7 +22,7 @@ public class NewUpdateTaskPage extends JPanel {
 	private JTextField tfTaskDescription;
 	private JTextField tfTaskDeadline;
 	private JLabel lblErrorMessageDeadline;
-	private User user;
+	private transient User user;
 
 	private Task toBeUpdatedTask = null;
 	private JButton btnSave;
@@ -47,48 +47,48 @@ public class NewUpdateTaskPage extends JPanel {
 		
 		JLabel lblTaskName = new JLabel("Task Name");
 		lblTaskName.setName("lblTaskName");
-		GridBagConstraints gbc_lblTaskName = new GridBagConstraints();
-		gbc_lblTaskName.insets = new Insets(0, 0, 5, 0);
-		gbc_lblTaskName.gridx = 0;
-		gbc_lblTaskName.gridy = 1;
-		add(lblTaskName, gbc_lblTaskName);
+		GridBagConstraints lblTaskNameConstraints = new GridBagConstraints();
+		lblTaskNameConstraints.insets = new Insets(0, 0, 5, 0);
+		lblTaskNameConstraints.gridx = 0;
+		lblTaskNameConstraints.gridy = 1;
+		add(lblTaskName, lblTaskNameConstraints);
 		
 		tfTaskName = new JTextField();
 		tfTaskName.setName("tfTaskName");
-		GridBagConstraints gbc_tfTaskName = new GridBagConstraints();
-		gbc_tfTaskName.insets = new Insets(0, 0, 5, 0);
-		gbc_tfTaskName.fill = GridBagConstraints.HORIZONTAL;
-		gbc_tfTaskName.gridx = 0;
-		gbc_tfTaskName.gridy = 2;
-		add(tfTaskName, gbc_tfTaskName);
+		GridBagConstraints tfTaskNameConstraints = new GridBagConstraints();
+		tfTaskNameConstraints.insets = new Insets(0, 0, 5, 0);
+		tfTaskNameConstraints.fill = GridBagConstraints.HORIZONTAL;
+		tfTaskNameConstraints.gridx = 0;
+		tfTaskNameConstraints.gridy = 2;
+		add(tfTaskName, tfTaskNameConstraints);
 		tfTaskName.setColumns(10);
 
 		lblErrorMessageName = new JLabel("Error message");
 		lblErrorMessageName.setVisible(false);
 		lblErrorMessageName.setName("lblErrorMessageName");
 		lblErrorMessageName.setForeground(Color.RED);
-		GridBagConstraints gbc_lblErrorMessageName = new GridBagConstraints();
-		gbc_lblErrorMessageName.insets = new Insets(0, 0, 5, 0);
-		gbc_lblErrorMessageName.gridx = 0;
-		gbc_lblErrorMessageName.gridy = 3;
-		add(lblErrorMessageName, gbc_lblErrorMessageName);
+		GridBagConstraints lblErrorMessageNameConstraints = new GridBagConstraints();
+		lblErrorMessageNameConstraints.insets = new Insets(0, 0, 5, 0);
+		lblErrorMessageNameConstraints.gridx = 0;
+		lblErrorMessageNameConstraints.gridy = 3;
+		add(lblErrorMessageName, lblErrorMessageNameConstraints);
 		
 		JLabel lblTaskDescription = new JLabel("Task Description");
-		GridBagConstraints gbc_lblTaskDescription = new GridBagConstraints();
+		GridBagConstraints lblTaskDescriptionConstraints = new GridBagConstraints();
 		lblTaskDescription.setName("lblTaskDescription");
-		gbc_lblTaskDescription.insets = new Insets(0, 0, 5, 0);
-		gbc_lblTaskDescription.gridx = 0;
-		gbc_lblTaskDescription.gridy = 5;
-		add(lblTaskDescription, gbc_lblTaskDescription);
+		lblTaskDescriptionConstraints.insets = new Insets(0, 0, 5, 0);
+		lblTaskDescriptionConstraints.gridx = 0;
+		lblTaskDescriptionConstraints.gridy = 5;
+		add(lblTaskDescription, lblTaskDescriptionConstraints);
 		
 		tfTaskDescription = new JTextField();
 		tfTaskDescription.setName("tfTaskDescription");
-		GridBagConstraints gbc_tfTaskDescription = new GridBagConstraints();
-		gbc_tfTaskDescription.insets = new Insets(0, 0, 5, 0);
-		gbc_tfTaskDescription.fill = GridBagConstraints.HORIZONTAL;
-		gbc_tfTaskDescription.gridx = 0;
-		gbc_tfTaskDescription.gridy = 6;
-		add(tfTaskDescription, gbc_tfTaskDescription);
+		GridBagConstraints tfTaskDescriptionConstraints = new GridBagConstraints();
+		tfTaskDescriptionConstraints.insets = new Insets(0, 0, 5, 0);
+		tfTaskDescriptionConstraints.fill = GridBagConstraints.HORIZONTAL;
+		tfTaskDescriptionConstraints.gridx = 0;
+		tfTaskDescriptionConstraints.gridy = 6;
+		add(tfTaskDescription, tfTaskDescriptionConstraints);
 		tfTaskDescription.setColumns(10);
 		
 		JLabel lblErrorMessageDescription = new JLabel("Error message");
@@ -96,47 +96,47 @@ public class NewUpdateTaskPage extends JPanel {
 		lblErrorMessageDescription.setHorizontalAlignment(SwingConstants.LEFT);
 		lblErrorMessageDescription.setForeground(Color.RED);
 		lblErrorMessageDescription.setName("lblErrorMessageDescription");
-		GridBagConstraints gbc_lblErrorMessageDescription = new GridBagConstraints();
-		gbc_lblErrorMessageDescription.insets = new Insets(0, 0, 5, 0);
-		gbc_lblErrorMessageDescription.gridx = 0;
-		gbc_lblErrorMessageDescription.gridy = 7;
-		add(lblErrorMessageDescription, gbc_lblErrorMessageDescription);
+		GridBagConstraints lblErrorMessageDescriptionConstraints = new GridBagConstraints();
+		lblErrorMessageDescriptionConstraints.insets = new Insets(0, 0, 5, 0);
+		lblErrorMessageDescriptionConstraints.gridx = 0;
+		lblErrorMessageDescriptionConstraints.gridy = 7;
+		add(lblErrorMessageDescription, lblErrorMessageDescriptionConstraints);
 		
 		JLabel lblDeadline = new JLabel("Deadline (dd/MM/yyyy)");
 		lblDeadline.setName("lblTaskDeadline");
-		GridBagConstraints gbc_lblDeadline = new GridBagConstraints();
-		gbc_lblDeadline.insets = new Insets(0, 0, 5, 0);
-		gbc_lblDeadline.gridx = 0;
-		gbc_lblDeadline.gridy = 9;
-		add(lblDeadline, gbc_lblDeadline);
+		GridBagConstraints lblDeadlineConstraints = new GridBagConstraints();
+		lblDeadlineConstraints.insets = new Insets(0, 0, 5, 0);
+		lblDeadlineConstraints.gridx = 0;
+		lblDeadlineConstraints.gridy = 9;
+		add(lblDeadline, lblDeadlineConstraints);
 		
 		tfTaskDeadline = new JTextField();
 		tfTaskDeadline.setName("tfTaskDeadline");
-		GridBagConstraints gbc_tfDeadline = new GridBagConstraints();
-		gbc_tfDeadline.insets = new Insets(0, 0, 5, 0);
-		gbc_tfDeadline.fill = GridBagConstraints.HORIZONTAL;
-		gbc_tfDeadline.gridx = 0;
-		gbc_tfDeadline.gridy = 10;
-		add(tfTaskDeadline, gbc_tfDeadline);
+		GridBagConstraints tfDeadlineConstraints = new GridBagConstraints();
+		tfDeadlineConstraints.insets = new Insets(0, 0, 5, 0);
+		tfDeadlineConstraints.fill = GridBagConstraints.HORIZONTAL;
+		tfDeadlineConstraints.gridx = 0;
+		tfDeadlineConstraints.gridy = 10;
+		add(tfTaskDeadline, tfDeadlineConstraints);
 		tfTaskDeadline.setColumns(10);
 
 		lblErrorMessageDeadline = new JLabel("Error message");
 		lblErrorMessageDeadline.setVisible(false);
 		lblErrorMessageDeadline.setName("lblErrorMessageDeadline");
 		lblErrorMessageDeadline.setForeground(Color.RED);
-		GridBagConstraints gbc_lblErrorMessageDeadline = new GridBagConstraints();
-		gbc_lblErrorMessageDeadline.insets = new Insets(0, 0, 5, 0);
-		gbc_lblErrorMessageDeadline.gridx = 0;
-		gbc_lblErrorMessageDeadline.gridy = 11;
-		add(lblErrorMessageDeadline, gbc_lblErrorMessageDeadline);
+		GridBagConstraints lblErrorMessageDeadlineConstraints = new GridBagConstraints();
+		lblErrorMessageDeadlineConstraints.insets = new Insets(0, 0, 5, 0);
+		lblErrorMessageDeadlineConstraints.gridx = 0;
+		lblErrorMessageDeadlineConstraints.gridy = 11;
+		add(lblErrorMessageDeadline, lblErrorMessageDeadlineConstraints);
 
 		btnSave = new JButton("Save");
 		btnSave.setName("btnSave");
 		btnSave.addActionListener(this::saveTask);
-		GridBagConstraints gbc_btnSave = new GridBagConstraints();
-		gbc_btnSave.gridx = 0;
-		gbc_btnSave.gridy = 13;
-		add(btnSave, gbc_btnSave);
+		GridBagConstraints btnSaveConstraints = new GridBagConstraints();
+		btnSaveConstraints.gridx = 0;
+		btnSaveConstraints.gridy = 13;
+		add(btnSave, btnSaveConstraints);
 	}
 
 	public boolean isUpdateMode(){
