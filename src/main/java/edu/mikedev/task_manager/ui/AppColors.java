d16f6275f3f9430e239cf3b6f5b0b676e8f662d2
mikedev <mik3dev@gmail.com>
2022-03-24 17:49:01 +0100

    change green rgb values

--- src/main/java/edu/mikedev/task_manager/ui/AppColors.java
+++ src/main/java/edu/mikedev/task_manager/ui/AppColors.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class AppColors {
     public static final Color RED = new Color(255, 107, 108);
     public static final Color ORANGE = new Color(249,212,35);
-    public static final Color GREEN = new Color(225,245,196);
+    public static final Color GREEN = new Color(185, 234, 186);
 
     private AppColors(){}
 
